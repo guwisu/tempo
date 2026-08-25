68b0327b96daa26a8abc11de0d562bb12dc6c84b
guwisu <kolyabelykov@mail.ru>
2026-08-26 00:01:27 +0400

    add stop, status and today commands

--- src/tempo/cli.py
+++ src/tempo/cli.py
@@ -6,21 +6,28 @@ app = typer.Typer(
 )
 
 
-@app.callback()
-def main():
-    """Tempo - time tracking CLI."""
-
-
-@app.command()
-def stop(activity: str):
-    """Stop tracking an activity."""
-    typer.echo(f"Stopping activity: {activity}.")
-
-
 @app.command()
 def start(activity: str):
     """Start tracking an activity."""
     typer.echo(f"Starting activity: {activity}.")
+
+
+@app.command()
+def stop():
+    """Stop the current activity."""
+    typer.echo("Stopping current activity...")
+
+
+@app.command()
+def status():
+    """Shows your status."""
+    typer.echo("Your activity: ...")
+
+
+@app.command()
+def today():
+    """Shows today's activity."""
+    typer.echo("Your today's activity.")
 
 
 if __name__ == "__main__":
