51ed2600a18e4ebd5198c373f1f7f78af406f020
guwisu <kolyabelykov@mail.ru>
2026-09-16 21:00:03 +0400

    change settings

--- src/tempo/config.py
+++ src/tempo/config.py
@@ -1,13 +1,21 @@
 import os
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from pathlib import Path
 
 class Settings(BaseSettings):
-    database_url: str = os.getenv(
-        "DATABASE_URL",
-        "postgresql+psycopg://postgres:postgres@localhost:5432/postgres"
-        )
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
 
+    @property
+    def database_url(self):
+        return f"postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+    model_config = SettingsConfigDict(env_file=f"{Path(__file__).parent.parent.parent / '.env'}")
 
 settings = Settings()
