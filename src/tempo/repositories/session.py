dc8c2f9b0e4a33339723f5c1dcac1a6664ae9f2e
guwisu <kolyabelykov@mail.ru>
2026-09-17 21:20:21 +0400

    add some functions for session

--- src/tempo/repositories/session.py
+++ src/tempo/repositories/session.py
@@ -1,4 +1,16 @@
 
 
 class SessionRepository():
-    ...
+
+
+    def get_active_session(self):
+        ...
+
+    def create_session(self):
+        ...
+
+    def finish_session(self):
+        ...
+
+    def get_today_sessions(self):
+        ...
